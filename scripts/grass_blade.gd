e9cabb4197f211d6cfda226297b7faaa951b0c8f
Baerkanogue <baerkanogue@gmail.com>
2025-09-02 16:46:52 +0200

    Add Whisp character and shader integration
    Introduces the Whisp scene and its custom shader for visual effects. Updates grass_blade.gd to reference Whisp instead of Player. Renames grass.tres to grass_shader.tres for clarity.

--- scripts/grass_blade.gd
+++ scripts/grass_blade.gd
@@ -2,7 +2,7 @@
 extends MeshInstance3D
 
 
-@onready var player: CharacterBody3D = $"../../../Player"
+@onready var player: CharacterBody3D = $"../../../Whisp"
 @onready var mat: ShaderMaterial = self.mesh.surface_get_material(0)
 
 func _physics_process(_delta: float) -> void:
