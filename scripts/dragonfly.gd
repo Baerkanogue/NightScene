b8e09646e58ae374c49e23101ac114d9afb83653
Baerkanogue <baerkanogue@gmail.com>
2025-09-10 13:49:12 +0200

    dragonfly fix + leaves particles initial

--- scripts/dragonfly.gd
+++ scripts/dragonfly.gd
@@ -6,6 +6,9 @@ extends Node3D
 
 var markers_array: Array[Marker3D]
 var is_move_ready: bool = true
+var new_pos: Vector3
+var pos_timer: Timer = Timer.new()
+var reached_distance: float = 0.001
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
@@ -18,10 +21,9 @@ func _ready() -> void:
 	
 	markers_array.append_array(markers_node.get_children())
 	
-	var pos_timer: Timer = Timer.new()
 	self.add_child(pos_timer)
 	pos_timer.timeout.connect(_on_pos_timer)
-	pos_timer.start(wait_time)
+	pos_timer.one_shot = true
 
 
 func _process(delta: float) -> void:
@@ -29,16 +31,19 @@ func _process(delta: float) -> void:
 
 
 func dragonfly(delta: float) -> void:
-	var chosen_marker: Marker3D = markers_array.pick_random()
-	var new_pos: Vector3
-	self.rotate_y(randf() * TAU)
 	if is_move_ready:
+		self.rotate_y(randf() * TAU)
+		var chosen_marker: Marker3D = markers_array.pick_random()
 		new_pos = chosen_marker.global_position
 		is_move_ready = false
-	self.global_position = lerp(self.global_position, new_pos, 10 * delta)
-	if self.global_position == new_pos:
-		print("ee")
+	else:
+		self.global_position = lerp(self.global_position, new_pos, lerp_speed * delta)
+	
+	if self.global_position.distance_to(new_pos) < reached_distance:
+		if pos_timer.is_stopped():
+			pos_timer.start(wait_time)
 
 
 func _on_pos_timer() -> void:
 	is_move_ready = true
+	print("e")
